ec725873d070078fbe93ca8289356d6751fdaa65
sabari570 <rsabarisai2000@gmail.com>
2024-07-12 22:17:32 +0530

    added some comments for better understanding

--- src/main/java/com/example1/batch/BatchApplication.java
+++ src/main/java/com/example1/batch/BatchApplication.java
@@ -29,15 +29,18 @@ public class BatchApplication {
 		System.out.println("Starting...");
 	}
 
+	// API ENDPOINT: http://localhost:8080/convert
+	// HEADER TYPE: Content-Type: application/xml
+	// INPUT CONTENT: Inside input.xml file
 	@PostMapping("/convert")
 	public ResponseEntity<String> convertXmlToJson(@RequestBody String xml) throws XMLStreamException, IOException {
 		try {
-            converter.convertXmlToJson(xml);
-            return ResponseEntity.ok("XML converted to JSON successfully and written to output.json");
-        } catch (XMLStreamException | IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body("Failed to convert XML to JSON: " + e.getMessage());
-        }
+			converter.convertXmlToJson(xml);
+			return ResponseEntity.ok("XML converted to JSON successfully and written to output.json");
+		} catch (XMLStreamException | IOException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Failed to convert XML to JSON: " + e.getMessage());
+		}
 	}
 
 }
